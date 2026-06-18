cc5a61f01282969110a85cf6d00018809cda744d
Wanderson Passos Barcelos <wp.barcelos@gmail.com>
2026-06-18 12:07:56 +0000

    criado componente jokenpo

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Jokenpo from "./components/Jokenpo";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ola mundo</Text>
       <StatusBar style="auto" />
+      <Jokenpo />
     </View>
   );
 }
---
+++ components/Jokenpo.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Jokenpo() {
+  return (
+    <View>
+      <Text>Jokenpo</Text>
+    </View>
+  );
+}
